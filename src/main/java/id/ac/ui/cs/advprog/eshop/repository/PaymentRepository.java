bb59a8be1944ed0416ac0aa1d878fcb5409dd95c
bddaax <brenda147052gmail.com>
2025-03-07 17:04:17 +0700

    [GREEN] Implement PaymentRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -9,8 +9,42 @@ import java.util.List;
 @Repository
 public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
-    public Payment save(Payment payment) {}
-    public Payment findById(String id) {}
-    public List<Payment> findByOrderId(String orderId) {}
-    public List<Payment> findAll() {}
+
+    public Payment save(Payment payment) {
+        int i = 0;
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(payment.getId())) {
+                paymentData.remove(i);
+                paymentData.add(i, payment);
+                return payment;
+            }
+            i += 1;
+        }
+
+        paymentData.add(payment);
+        return payment;
+    }
+
+    public Payment findById(String id) {
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(id)) {
+                return savedPayment;
+            }
+        }
+        return null;
+    }
+
+    public List<Payment> findByOrderId(String orderId) {
+        List<Payment> result = new ArrayList<>();
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getOrderId().equals(orderId)) {
+                result.add(savedPayment);
+            }
+        }
+        return result;
+    }
+
+    public List<Payment> findAll() {
+        return new ArrayList<>(paymentData);
+    }
 }
